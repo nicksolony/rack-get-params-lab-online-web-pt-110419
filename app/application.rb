58766f8f13a235d0d887f97d7977d8ebd9654639
Nick Solonyy <nicksolony@gmail.com>
2020-02-23 19:33:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +12,8 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      if @@cart.empty? {puts 'Your cart is empty'}
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
