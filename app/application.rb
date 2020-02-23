40a415a248145318ef6cc6445e57d088119a591c
Nick Solonyy <nicksolony@gmail.com>
2020-02-23 19:41:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,7 @@ class Application
       end
     elsif req.path.match(/add/)
       resp.get = item
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
